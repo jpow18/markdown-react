504d5d0345294096f813d79b6451465ea6c99f22
James Pow <jbpow18@icloud.com>
2022-12-27 18:19:02 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactMarkdown from 'react-markdown'
+import {useState} from 'react';
+
 
 function App() {
+
+  const [input, setInput] = useState(
+    '![image of code](https://clipground.com/images/coding-16.jpg)'
+    + '\n# header'
+    + '\n## subheader'
+    + '\nThis site was built using[GitHub Pages](https://pages.github.com/).\n\n'
+    + '`this is inline code`\n\n'
+    + '```\ncode block\n```'
+    + '\n - this is a list item'
+    + '\n > this is a block quote'
+  + '\n\n this is **bold** text\n'
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        autoFocus
+        id="editor"
+        value={input}
+        breaks='true'
+        onChange={
+        (e) => setInput(e.target.value)
+      } />
+      <div id="preview" breaks='true'>
+        <ReactMarkdown children={input} />
+      </div>
     </div>
   );
 }
